20b998daa9d6bdccb947cb72da915d7055aab4e7
HaseebJaffri <85227965+HaseebJaffri@users.noreply.github.com>
2023-10-31 22:58:30 +0500

    levels

--- InksAndOrbs/Assets/Scripts/OcularInk/Models/UserData.cs
+++ InksAndOrbs/Assets/Scripts/OcularInk/Models/UserData.cs
@@ -33,6 +33,34 @@ public struct UserData
                 return Score.Level9;
             case 9:
                 return Score.Level10;
+            case 10:
+                return Score.Level11;
+            case 11:
+                return Score.Level12;
+            case 12:
+                return Score.Level13;
+            case 13:
+                return Score.Level14;
+            case 14:
+                return Score.Level15;
+            case 15:
+                return Score.Level16;
+            case 16:
+                return Score.Level17;
+            case 17:
+                return Score.Level18;
+            case 18:
+                return Score.Level19;
+            case 19:
+                return Score.Level20;
+            case 20:
+                return Score.Level21;
+            case 21:
+                return Score.Level22;
+            case 22:
+                return Score.Level23;
+            case 23:
+                return Score.Level24;
         }
 
         return 0;
@@ -52,4 +80,18 @@ public struct ScoreData
     [FirestoreProperty] public int Level8 { get; set; }
     [FirestoreProperty] public int Level9 { get; set; }
     [FirestoreProperty] public int Level10 { get; set; }
+    [FirestoreProperty] public int Level11 { get; set; }
+    [FirestoreProperty] public int Level12 { get; set; }
+    [FirestoreProperty] public int Level13 { get; set; }
+    [FirestoreProperty] public int Level14 { get; set; }
+    [FirestoreProperty] public int Level15 { get; set; }
+    [FirestoreProperty] public int Level16 { get; set; }
+    [FirestoreProperty] public int Level17 { get; set; }
+    [FirestoreProperty] public int Level18 { get; set; }
+    [FirestoreProperty] public int Level19 { get; set; }
+    [FirestoreProperty] public int Level20 { get; set; }
+    [FirestoreProperty] public int Level21 { get; set; }
+    [FirestoreProperty] public int Level22 { get; set; }
+    [FirestoreProperty] public int Level23 { get; set; }
+    [FirestoreProperty] public int Level24 { get; set; }
 }
--- InksAndOrbs/Assets/Scripts/OcularInk/Utils/SceneMaster.cs
+++ InksAndOrbs/Assets/Scripts/OcularInk/Utils/SceneMaster.cs
@@ -60,5 +60,5 @@ public class SceneMaster
 
 public enum GameScene
 {
-    Loading, Home, Shards, Level1, Level2, Level3, Level4, Level5, Level6, Level7, Level8, Level9, Level10
+    Loading, Home, Shards, Level1, Level2, Level3, Level4, Level5, Level6, Level7, Level8, Level9, Level10, Level11, Level12, Level13, Level14, Level15, Level16, Level17, Level18, Level19, Level20, Level21, Level22, Level23, Level24
 }
